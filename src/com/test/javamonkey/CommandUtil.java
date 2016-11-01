2a991fd21f67a7b4068ec9896376ecae651df9ab
hi-cbh <hi_cbh@qq.com>
2016-11-01 16:54:03 +0800

    完成调试模块

--- src/com/test/javamonkey/CommandUtil.java
+++ src/com/test/javamonkey/CommandUtil.java
@@ -7,10 +7,10 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
+import java.util.Scanner;
 
 /***
- * ÷¥––√¸¡Ó––π§æﬂ¿‡
+ * ÊâßË°åÂëΩ‰ª§Ë°åÂ∑•ÂÖ∑Á±ª
  * 
  * @author yangxiaolong 2014-04-30
  * 
@@ -18,13 +18,12 @@ import java.util.Locale;
 public class CommandUtil {
 
 	public static final String TAG = CommandUtil.class.getSimpleName();
-	public static final String COMMAND_SH = "";
+	public static final String COMMAND_SH = "cmd /c";
 	public static final String COMMAND_LINE_END = "\n";
 	public static final String COMMAND_EXIT = "exit\n";
 
-
 	/**
-	 * ÷¥––µ•Ãı√¸¡Ó
+	 * ÊâßË°åÂçïÊù°ÂëΩ‰ª§
 	 * 
 	 * @param command
 	 * @return
@@ -34,7 +33,7 @@ public class CommandUtil {
 	}
 
 	/**
-	 * ø…÷¥––∂‡––√¸¡Ó£®bat£©
+	 * ÂèØÊâßË°åÂ§öË°åÂëΩ‰ª§ÔºàbatÔºâ
 	 * 
 	 * @param commands
 	 * @return
@@ -106,33 +105,42 @@ public class CommandUtil {
 				process.destroy();
 			}
 		}
-		System.out.println(String.format(Locale.CHINA,  
-                "execute command end,errorMsg:%s,and status %d: ", errorMsg,  
-                status)); 
+		System.out.println(String.format(Locale.CHINA,
+				"execute command end,errorMsg:%s,and status %d: ", errorMsg,
+				status));
 		return results;
 	}
 
-	
-	
-	public static void execCommand2(String command) throws IOException {
-		Runtime runtime = Runtime.getRuntime();
-		Process proc = runtime.exec(command);
+	public static void execCommand2(String command) {
+		System.out.println("execCommand2");
+		Runtime runtime = null;
+		Process proc = null;
+
 		try {
+
+			runtime = Runtime.getRuntime();
+			System.out.println("before");
+			proc = runtime.exec(command);
+			System.out.println("after");
 			if (proc.waitFor() != 0) {
 				System.err.println("exit value = " + proc.exitValue());
 			}
+			System.out.println("run");
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					proc.getInputStream()));
 			StringBuffer stringBuffer = new StringBuffer();
 			String line = null;
+			System.out.println("wait for");
 			while ((line = in.readLine()) != null) {
-				stringBuffer.append(line);
+				// stringBuffer.append(line);
 				System.out.println(line);
 			}
-			
-
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (InterruptedException e) {
-			System.err.println(e);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} finally {
 			try {
 				proc.destroy();
@@ -141,6 +149,99 @@ public class CommandUtil {
 		}
 	}
 
-	
+	public static List<String> execCommand(String command) {
+		Runtime runtime = null;
+		Process proc = null;
+		WatchThread wt = null;
+		ArrayList<String> commandStream = null;
+		try {
+			runtime = Runtime.getRuntime();
+			proc = runtime.exec(command);
 
+			wt = new WatchThread(proc);
+			wt.start();
+
+			if (proc.waitFor() != 0) {
+				System.err.println("exit value = " + proc.exitValue());
+			}
+			commandStream = wt.getStream();
+			wt.setOver(true);
+
+			//System.out.println("commandStream: " + commandStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				proc.destroy();
+			} catch (Exception e2) {
+			}
+		}
+		
+		return commandStream;
+	}
+
+	public static class WatchThread extends Thread {
+		Process p;
+		boolean over;
+		ArrayList<String> stream;
+
+		public WatchThread(Process p) {
+			this.p = p;
+			over = false;
+			stream = new ArrayList<String>();
+		}
+
+		public void run() {
+			try {
+				if (p == null)
+					return;
+				Scanner br = new Scanner(p.getInputStream());
+				while (true) {
+					if (p == null || over)
+						break;
+					while (br.hasNextLine()) {
+						String tempStream = br.nextLine();
+						if (tempStream.trim() == null
+								|| tempStream.trim().equals(""))
+							continue;
+						stream.add(tempStream);
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void setOver(boolean over) {
+			this.over = over;
+		}
+
+		public ArrayList<String> getStream() {
+			return stream;
+		}
+	}
+
+	
+	public static List<String> getPackage(List<String> stream){
+		List<String> results = new ArrayList<String>();
+		int status = -1;
+		if (stream == null || stream.size() == 0) {
+			return null;
+		}
+		
+		for(String line : stream){
+			
+			if(line.contains("package:")){
+				String str = line.substring(8, line.indexOf(" "));
+				System.out.println("line : " + str);
+				results.add(str);
+			}
+		}
+		
+		return results;
+	}
+	
+	
 }
--- src/com/test/javamonkey/MainMonkey.java
+++ src/com/test/javamonkey/MainMonkey.java
@@ -6,6 +6,12 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -20,6 +26,8 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 
 
@@ -32,15 +40,33 @@ public class MainMonkey extends JFrame implements ActionListener{
 	JComboBox<String> jcb = null;
 	JButton add, jb1, jb2, jb3, jb4, jb5, jb6, jb7 = null;
 	JTextField jtf1, jtf2, jtf3=null;
-	JCheckBox jcb1 ,jcb2, jcb3, jcb4, jcb5,jcb6, jcb7, jcb8,jcb9,jcb10 = null;
+	JCheckBox jcb1 ,jcb2, jcb3, jcb4, jcb5,jcb6, jcb7, jcb8,jcb9,jcb10, jcb11= null;
 	JSlider js1 ,js2, js3, js4,js5, js6, js7, js8= null;
 	Box slider = new Box(BoxLayout.Y_AXIS);
 	
+	
+	
 	Font f16 = new Font("",Font.BOLD,16);
 	public static String cmdline = "adb shell monkey ";
-	public static String logStr = "";
+	public static String logline = "-v -v -v";
+	public static String packageline = "";
+	public static String seedline = "";
+	public static String timeline = "1000"; //间隔时间默认1秒
+	public static String debugline = "";
+	
+	public static String runline = "1000";
 	
 	public String[] list = {" "};
+	List<String> stream = null;
+	List<String> bllist = null;
+	static int debuglist[] = {1,1,1,1,1};
+	static String[] debugStrList = {
+		"--ignore-crashes",
+		"--ignore-timeouts",
+		"--ignore-security-exceptions",
+		"--kill-process-after-error",
+		"--randomize-throttle"
+	};
 	
 	
 	public static void main(String[] args) {
@@ -90,24 +116,21 @@ public class MainMonkey extends JFrame implements ActionListener{
                 if (radio == logjrb1) {
                 	logtmp = " -v ";
                 	
-                    System.out.println("You selected Radio 1");
+                   // System.out.println("You selected Radio 1");
                 } else if (radio == logjrb2) {
                 	logtmp = " -v -v ";
-                	System.out.println("You selected Radio 2");
+                	//System.out.println("You selected Radio 2");
                 } else {
                 	logtmp = " -v -v -v ";
-                	System.out.println("You selected Radio 3");
+                	//System.out.println("You selected Radio 3");
                 }
-                logStr = logtmp;
+                logline = logtmp;
             }
         };
 		
         logjrb1.addActionListener(al);
         logjrb2.addActionListener(al);
         logjrb3.addActionListener(al);
-        
-		
-		
 		
 		jp1.add(logjl);
 		jp1.add(logjrb1);
@@ -115,10 +138,6 @@ public class MainMonkey extends JFrame implements ActionListener{
 		jp1.add(logjrb3);
 		jp1.setLayout(new FlowLayout(FlowLayout.LEFT));
 		this.add(jp1);
-		
-		
-		
-		
 		
 		//运行的包名
 		//运行包名
@@ -130,13 +149,27 @@ public class MainMonkey extends JFrame implements ActionListener{
 		jp2.add(add);
 		jp2.setLayout(new FlowLayout(FlowLayout.LEFT));
 		this.add(jp2);
-		
+		System.out.println("action listenter");
 		add.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				CommandUtil.execute("adb shell pm list package -3 -i com");
+//				String[] tmp = {"adb","shell","pm","list","package","-3","-i","com"};
+//				CommandUtil.execute(tmp);
+				//System.out.println("put down");
+				stream = CommandUtil.execCommand("adb shell pm list package -s -i calc");
 				
+				List<String> packagelist =  CommandUtil.getPackage(stream);
+				
+				if(packagelist == null || packagelist.size() == 0){
+					return;
+				}
+				
+				int count = packagelist.size();
+				
+				for(int i = 0; i < count; i++){
+					jcb.addItem(packagelist.get(i));
+				}
 			}
 		});
 		
@@ -145,14 +178,12 @@ public class MainMonkey extends JFrame implements ActionListener{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
+				packageline = " -p "+ jcb.getSelectedItem().toString().trim();
 				
 			}
 		});
 		
-		
-		
-		
-		
+
 		//运行种子 seed
 		jl2=new JLabel("随机种子：");
 		jtf1=new JTextField(10);
@@ -162,17 +193,84 @@ public class MainMonkey extends JFrame implements ActionListener{
 		jp3.add(jtf1);
 		jp3.setLayout(new FlowLayout(FlowLayout.LEFT));
 		this.add(jp3);
+		jcb1.setSelected(true);
+		jtf1.setEditable(false);
+		
+		jcb1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JCheckBox tmpjcb = (JCheckBox) e.getSource();
+				
+				if(tmpjcb.isSelected()){
+					jtf1.setEditable(false);
+					jtf1.setText("");
+					seedline = "";
+				}
+				else{
+					jtf1.setEditable(true);
+					
+				}
+			}
+		});
+		
+		jtf1.addKeyListener(new KeyAdapter(){  
+            public void keyTyped(KeyEvent e) {  
+                int keyChar = e.getKeyChar();                 
+                if(keyChar >= KeyEvent.VK_0 && keyChar <= KeyEvent.VK_9){  
+                      
+                }else{  
+                    e.consume(); //关键，屏蔽掉非法输入  
+                }  
+            }  
+        });
+		
 		
 		//间隔时间
 		jl3=new JLabel("间隔时间：");
 		jtf2=new JTextField(10);
-		jcb2 = new JCheckBox("间隔时间");
+		jcb2 = new JCheckBox("默认");
 		jp4.add(jl3);
 		jp4.add(jcb2);
 		jp4.add(jtf2);
 		jp4.setLayout(new FlowLayout(FlowLayout.LEFT));
 		this.add(jp4);
-				
+		
+		jcb2.setSelected(true);
+		jtf2.setEditable(false);
+		jtf2.setText(timeline);
+		
+		jcb2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JCheckBox tmpjcb = (JCheckBox) e.getSource();
+				//复选框，选中
+				if(tmpjcb.isSelected()){
+					jtf2.setEditable(false);
+					jtf2.setText("1000");
+					timeline = "1000";
+				}
+				else{
+					jtf2.setEditable(true);
+					jtf2.setText("");
+					timeline = "";
+				}
+			}
+		});
+		
+		jtf2.addKeyListener(new KeyAdapter(){  
+            public void keyTyped(KeyEvent e) {  
+                int keyChar = e.getKeyChar();                 
+                if(keyChar >= KeyEvent.VK_0 && keyChar <= KeyEvent.VK_9){  
+                      
+                }else{  
+                    e.consume(); //关键，屏蔽掉非法输入  
+                }  
+            }  
+        });
+		
+
 		jp5.setBorder(BorderFactory.createTitledBorder("调试"));
 		jcb3 = new JCheckBox("ignore-crashes");
 		jcb4 = new JCheckBox("ignore-timeouts");
@@ -186,6 +284,13 @@ public class MainMonkey extends JFrame implements ActionListener{
 		jp5.add(jcb7);
 		jp5.setLayout(new GridLayout(3,2));
 		this.add(jp5);
+		
+		jcb3.addItemListener(new MyItemListener());
+		jcb4.addItemListener(new MyItemListener());
+		jcb5.addItemListener(new MyItemListener());
+		jcb6.addItemListener(new MyItemListener());
+		jcb7.addItemListener(new MyItemListener());
+
 		
 		
 		
@@ -222,15 +327,29 @@ public class MainMonkey extends JFrame implements ActionListener{
 		//jp6.setLayout(new GridLayout(1,8));
 		jp9.setLayout(new BoxLayout(jp9,BoxLayout.X_AXIS));
 		
+		js1.setValue(80);
+		js2.setValue(20);
+		js3.setValue(0);
+		js4.setValue(0);
+		js5.setValue(0);
+		js6.setValue(0);
+		
+		js1.addChangeListener(new MyChageListener());
+		js2.addChangeListener(new MyChageListener());
+		js3.addChangeListener(new MyChageListener());
+		js4.addChangeListener(new MyChageListener());
+		js5.addChangeListener(new MyChageListener());
+		js6.addChangeListener(new MyChageListener());
+		
+		
+		
 		jl6 = new JLabel("touch");
 		jl7 = new JLabel("motion");
 		jl8 = new JLabel("trackball");
 		jl9 = new JLabel("nav");
 		jl10 = new JLabel("appswitch");
 		jl11 = new JLabel("anyevent");
-		
 
-		
 		jp10.add(jl6);
 		jp10.add(Box.createVerticalStrut(x));
 		jp10.add(jl7);
@@ -248,6 +367,12 @@ public class MainMonkey extends JFrame implements ActionListener{
 		jp6.add(jp10);
 		this.add(jp6);
 		
+		
+		
+		
+		
+		
+		
 		//快捷方案
 		jl4=new JLabel("快捷测试方案：");
 		jb1 = new JButton("方案一");
@@ -260,14 +385,49 @@ public class MainMonkey extends JFrame implements ActionListener{
 		jp7.setLayout(new FlowLayout(FlowLayout.LEFT));
 		this.add(jp7);
 		
-		jl5 = new JLabel("运行测试: ");
-		jtf3 = new JTextField(20);
+		jl5 = new JLabel("事件数量: ");
+		jcb11 = new JCheckBox("默认");
+		jtf3 = new JTextField(16);
 		jp8.add(jl5);
+		jp8.add(jcb11);
 		jp8.add(jtf3);
 		jp8.setLayout(new FlowLayout(FlowLayout.LEFT));
 		this.add(jp8);
 		
+		jcb11.setSelected(true);
+		jtf3.setEditable(false);
+		jtf3.setText(runline);
 		
+		jcb11.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JCheckBox tmpjcb = (JCheckBox) e.getSource();
+				//复选框，选中
+				if(tmpjcb.isSelected()){
+					jtf3.setEditable(false);
+					jtf3.setText("1000");
+					runline = "1000";
+				}
+				else{
+					jtf3.setEditable(true);
+					jtf3.setText("");
+					runline = "";
+				}
+			}
+		});
+		
+		jtf3.addKeyListener(new KeyAdapter(){  
+            public void keyTyped(KeyEvent e) {  
+                int keyChar = e.getKeyChar();                 
+                if(keyChar >= KeyEvent.VK_0 && keyChar <= KeyEvent.VK_9){  
+                      
+                }else{  
+                    e.consume(); //关键，屏蔽掉非法输入  
+                }  
+            }  
+        });
+
 		jb5 = new JButton("Run");
 		jb6 = new JButton("Close");
 		jb7 = new JButton("Stop");
@@ -276,12 +436,58 @@ public class MainMonkey extends JFrame implements ActionListener{
 		this.add(jb6);
 		this.add(jb7);
 		
+		jb6.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			System.out.println("close");
+				 System.exit(0);
+			}
+		});
+		
+		
+		
 		jb5.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println(cmdline + logStr);
 				
+				if(jtf1.isEditable() && !jtf1.getText().equals("")){
+					seedline = " -s " + jtf1.getText();
+				}
+				else if(jtf1.isEditable() && jtf1.getText().equals("")){
+					seedline = "";
+				}
+				
+				//间隔时间必须有，默认为1000（1秒）
+				if(jtf2.isEditable() && !jtf2.getText().equals("")){
+					timeline = " --throttle " + jtf2.getText();
+				}
+				//输入框不可写，内容不为空
+				else if(!jtf2.isEditable() && jtf2.getText().equals("1000")){
+					timeline = " --throttle 1000";
+				}
+				else if(jtf2.isEditable() && jtf2.getText().equals("")){
+					timeline = " --throttle 1000";
+				}
+				
+				
+				//事件次数
+				if(jtf3.isEditable() && !jtf3.getText().equals("")){
+					runline = " " + jtf3.getText();
+				}
+				//输入框不可写，内容不为空
+				else if(!jtf3.isEditable() && jtf3.getText().equals("1000")){
+					runline = " " + jtf3.getText();
+				}
+				else if(jtf3.isEditable() && jtf3.getText().equals("")){
+					runline = " 1000";
+				}
+				
+				debugline = getDebugStr();
+				//System.out.println("---------next()-----------");
+				System.out.println(cmdline + logline + packageline + seedline + timeline + debugline + runline);
+				CommandUtil.execCommand(cmdline + logline + packageline + seedline + timeline + debugline + runline);
 			}
 			
 		});
@@ -319,7 +525,7 @@ public class MainMonkey extends JFrame implements ActionListener{
 		//设置窗体大小
 		this.setSize(400,600);
 		//设置窗体初始位置
-		this.setLocation(200, 150);
+		this.setLocation(100, 50);
 		//设置当关闭窗口时，保证JVM也退出
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//显示窗体
@@ -327,6 +533,51 @@ public class MainMonkey extends JFrame implements ActionListener{
 		this.setResizable(false);
 		
 	}
+
+	
+	class MyItemListener implements ItemListener {  
+
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			JCheckBox source = (JCheckBox)e.getItemSelectable();
+			 if(source == jcb3){
+				debuglist[0]=changeNumber(source.isSelected());
+			 }else if(source == jcb4){
+				 debuglist[1]=changeNumber(source.isSelected());
+			 }else if(source == jcb5){
+				 debuglist[2]=changeNumber(source.isSelected());
+			 }else if(source == jcb6){
+				 debuglist[3]=changeNumber(source.isSelected());
+			 }else if(source == jcb7){
+				 debuglist[4]=changeNumber(source.isSelected());
+			 }
+		} 
+	}
+	
+	
+	class MyChageListener implements ChangeListener{
+
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			JSlider js0 =  (JSlider)e.getSource();
+			if(js0 == js1){
+				
+			}else if(js0 == js2){
+				
+			}else if(js0 == js3){
+				
+			}else if(js0 == js4){
+				
+			}else if(js0 == js5){
+				
+			}else if(js0 == js6){
+				
+			}
+		}
+		
+	}
+	
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -341,16 +592,58 @@ public class MainMonkey extends JFrame implements ActionListener{
 		}
 		else if(e.getActionCommand()=="Close")
 		{
-                 
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		}	
 		
 	}
 	
-	public void addSlider(JSlider slider, String des){
-		Box box = new Box(BoxLayout.PAGE_AXIS);
-		box.add(slider);
-		box.add(new JLabel(des));
+
+	
+	public int changeNumber(boolean bl){
 		
+		if(bl == true){
+			return 0;
+		}else{
+			return 1;
+		}
 	}
 
+	
+
+	public List<String> getDebugList(){
+		int cnt = debuglist.length;
+		ArrayList<String> list = new ArrayList<String>();
+		for(int i = 0; i< cnt; i++){
+			if(debuglist[i] == 0){
+				list.add(debugStrList[i]);
+			}
+		}
+		
+		for(String str : list){
+			System.out.println("str: " + str);
+		}
+		
+		return list;
+	}
+	
+	public String getDebugStr(){
+		int cnt = debuglist.length;
+		String tmp =" ";
+		ArrayList<String> list = new ArrayList<String>();
+		for(int i = 0; i< cnt; i++){
+			if(debuglist[i] == 0){
+				list.add(debugStrList[i]);
+			}
+		}
+		
+		if(list.size() == 0){
+			return "";
+		}
+		
+		for(String str : list){
+			tmp = tmp + str + " ";
+		}
+		System.out.println("tmp: " + tmp);
+		return tmp;
+	}
 }
